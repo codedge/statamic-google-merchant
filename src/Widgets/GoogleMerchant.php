0d405920b1e88ab97f39b0f82cddcc50111bb74e
codedge <codedge@users.noreply.github.com>
2021-03-28 18:49:46 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Widgets/GoogleMerchant.php
+++ src/Widgets/GoogleMerchant.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Codedge\GoogleMerchant\Widgets;
 
 use Codedge\GoogleMerchant\Repositories\SettingsRepository;
-use Illuminate\Support\Facades\Storage;
 use SimpleXMLElement;
 use Statamic\Widgets\Widget;
 
@@ -22,12 +21,12 @@ final class GoogleMerchant extends Widget
 
         $feedFile = storage_path($this->settingsRepository->filename());
 
-        if(file_exists($feedFile)) {
+        if (file_exists($feedFile)) {
             $xml = new SimpleXMLElement(file_get_contents($feedFile));
             $exists = true;
             $count = count($xml->channel->item);
         }
 
-        return view('gm::cp.widgets.google-merchant-feed', compact("exists", "count"));
+        return view('gm::cp.widgets.google-merchant-feed', compact('exists', 'count'));
     }
 }
